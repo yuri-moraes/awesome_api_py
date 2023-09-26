352650b15587a91a27a360b063b92a726c28bebf
Yuri Moraes <yuri.nogueira.de.moraes@hotmail.com>
2023-09-26 10:34:40 -0300

    Adicionando a exibição da cotação do euro e bitcoin para real brasileiro

--- awesome.py
+++ awesome.py
@@ -3,5 +3,11 @@ import json
 
 cotacoes = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
 cotacoes = cotacoes.json()
+
 cotacao_dolar = cotacoes['USDBRL']['bid']
-print(cotacao_dolar)
+cotacao_euro = cotacoes['EURBRL']['bid']
+cotacao_bitcoin = cotacoes['BTCBRL']['bid']
+
+print(f'Cotação do dólar: R${cotacao_dolar}')
+print(f'Cotação do euro: R${cotacao_euro}')
+print(f'Cotação do bitcoin: R${cotacao_bitcoin}')
